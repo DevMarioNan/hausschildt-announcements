35ae2deaebea5da34bc38de96a56c954539c11ff
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-02-21 21:48:30 +0200

    create page layout

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const Create = () => {
+const Create = ({user}) => {
+    const navigate = useNavigate();
+    if(!user){
+        navigate('/signin');
+    }
+    const handleForm = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Create</div>
+        <div className='mt-20'>
+            <h1 className='text-2xl font-bold text-center pb-10'>Create a new Announcement</h1>  
+            <form onSubmit={(e)=>handleForm(e)} className="flex flex-col justify-center gap-10">
+                <input type="text" placeholder="title" className='border shadow-sm w-[70%] mx-auto px-5 py-3 ' />
+                <textarea placeholder="description" className='border shadow-sm w-[70%] mx-auto px-5 py-3 ' />
+                <button type='submit' className='bg-blue-500 text-white px-5 py-3 rounded-lg mx-auto'>Create</button>
+            </form>
+        </div>
     )
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,11 @@ import Navbar from '../Navbar/Navbar'
 import Signin from '../Signin/Signin'
 import Announcements from '../Announcements/Announcements'
 import {Routes , Route} from 'react-router-dom'
+import Create from '../Create/Create'
 
 
 const Home = () => {
     const [user,setUser] = useState(null);
-    useEffect(() => {
-        console.log(user);
-    }, [user])
     
     
 
@@ -19,6 +17,7 @@ const Home = () => {
             <Routes>
                 <Route path="/" element={<Announcements />} />
                 <Route path="/signin" element={<Signin setUser={setUser} />} />
+                <Route path="/create" element={<Create user={user} />} />
             </Routes>
         </div>
     )
