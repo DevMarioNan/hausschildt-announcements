d69550a6b805ff5b3a3b436c95cd24e1e4be6e10
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-02-26 06:08:41 +0200

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Home from './components/Home/Home.jsx';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div>
       <Home />
+      <Analytics  />
     </div>
   );
 }
